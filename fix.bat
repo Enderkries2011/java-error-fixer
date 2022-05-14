6d518201dbf0e43d132034c5ba143f7fb14572f8
Enderkries2011 <88530711+Enderkries2011@users.noreply.github.com>
2022-05-14 20:04:27 +0200

    Add files via upload

---
+++ fix.bat
@@ -0,0 +1,34 @@
+@echo off
+
+
+:A
+cls
+echo.
+echo Welcome, to the java error fixer process...
+echo.
+set /p menu="Do you want to continue? (Y/N) : "
+if %menu%==Y goto Yes
+if %menu%==y goto Yes
+if %menu%==N goto No
+if %menu%==n goto No
+cls
+echo.
+echo Please answer me!...
+echo.
+set /p pause="Press any key to continue!..."
+goto A
+
+:Yes
+echo.
+echo Okay, lets begin the process
+echo.
+set /p pause="Press any key to continue!..."
+cd %USERPROFILE%\AppData\Roaming
+ren .minecraft random
+
+:No
+echo.
+echo Okay, canceling process
+echo.
+set /p pause="Press any key to continue!..."
+exit
